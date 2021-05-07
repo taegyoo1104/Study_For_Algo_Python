007f64a6c3e9beb782eb3f48f11024b8f35f4c08
taegyoo1104 <taegyoo1104@gmail.com>
2021-05-08 02:37:11 +0900

    이진 탐색

--- e_Binary_Search/Binary_Search_2.py
+++ e_Binary_Search/Binary_Search_2.py
@@ -1,2 +1,13 @@
 """ 이진 탐색_반복문 """
 
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
